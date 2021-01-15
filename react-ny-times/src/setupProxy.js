d6cb97d22f34b34932953464a4d48162a8e22c70
joyce_gao <joycegao@objectivasoftware.com>
2021-01-15 18:34:13 +0800

    NY Times

--- react-ny-times/src/setupProxy.js
+++ react-ny-times/src/setupProxy.js
@@ -1,17 +1,17 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
-const filter = function (pathname, req) {
-  return pathname.match("^/svc");
-};
-var restream = function (proxyReq, req, res, options) {
-  if (req.body) {
-    let bodyData = JSON.stringify(req.body);
-    // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
-    proxyReq.setHeader("Content-Type", "application/json");
-    proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
-    // stream the content
-    proxyReq.write(bodyData);
-  }
-};
+// const filter = function (pathname, req) {
+//   return pathname.match("^/svc");
+// };
+// var restream = function (proxyReq, req, res, options) {
+//   if (req.body) {
+//     let bodyData = JSON.stringify(req.body);
+//     // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+//     proxyReq.setHeader("Content-Type", "application/json");
+//     proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+//     // stream the content
+//     proxyReq.write(bodyData);
+//   }
+// };
 
 module.exports = function (app) {
   // app.use(
